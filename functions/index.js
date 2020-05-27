e81a8175dff25612106a600b80b8fee5f014ea10
MariaEneidaSalasMartínez <enesis141215@gmail.com>
2020-05-26 20:12:25 -0500

    Primer commit

---
+++ functions/index.js
@@ -0,0 +1,62 @@
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync')
+
+exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
+  console.log(">>>>>>INICIO")
+  const fileBucket = object.bucket; // The Storage bucket that contains the file.
+  const filePath = object.name; // File path in the bucket.
+  const contentType = object.contentType; // File content type.
+  const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+  console.log(filePath)
+  // [END eventAttributes]
+  // [START stopConditions]
+  // Exit if this is triggered on a file that is not an image.
+  //if (!contentType.startsWith('text/csv')) {
+  //  return console.log('Esto no es csv.');
+  // }
+  // [START thumbnailGeneration]
+  // Download file from bucket.
+  
+  const bucket = admin.storage().bucket(fileBucket);
+  const filename = path.basename(object.name);
+  console.log(">>>>>> ",  filename)
+
+  bucket.file(filename).download((err, contents) => {
+    if (err) {
+      console.log('error', err);
+      return null
+    }
+      console.log(">>>>>> ", contents);
+
+      toJSON(contents.toString());
+  });
+});
+
+function toJSON(content) {
+  const input = `
+"key_1","key_2"
+"value 1","value 2"
+`
+const records = parse(input, {
+  columns: true,
+  skip_empty_lines: true
+  
+})
+toDB(records)
+}
+
+function toDB(jsn) {
+admin.database().ref("plantilla").set(jsn);
+  // Here you can add you firestore/ realtime database or merge the JSON if you want to batch write.
+  console.log(jsn)
+}
